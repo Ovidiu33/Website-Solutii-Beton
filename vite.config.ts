b7bec97db42c43c0a24ede01ac4037d151283557
Ovidiu Taciuc <otaciuc@PMINTL.NET>
2026-02-05 21:18:37 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/Website-Solutii-Beton/',
   plugins: [react()],
   build: {
     outDir: 'dist',
